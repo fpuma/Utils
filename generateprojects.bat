984014fe4fa5b79dad3e8d17bfa60ca51cf2b7d3
fpuma <44748038+fpuma@users.noreply.github.com>
2020-11-15 15:16:38 +0100

    Initial commit

---
+++ generateprojects.bat
@@ -0,0 +1,4 @@
+@echo off
+cd sharpmake
+sharpmakeApplication\Sharpmake.Application.exe /sources(@"sharpmakeFiles\sharpmake.main.cs")
+pause
---
+++ sharpmake/sharpmakeFiles/generatedebugsharpmakesolution.bat
@@ -0,0 +1,3 @@
+@echo off
+..\sharpmakeApplication\Sharpmake.Application.exe /sources(@"..\sharpmakeFiles\sharpmake.main.cs") /generateDebugSolution
+pause
