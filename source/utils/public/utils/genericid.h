3ab685c6cc76b1234eda8bd38c68c9aa272b2f29
fpuma <fpuma88@gmail.com>
2021-01-31 13:39:25 +0100

    Added binary operators for GenericID

--- source/utils/public/utils/genericid.h
+++ source/utils/public/utils/genericid.h
@@ -10,7 +10,10 @@ namespace puma
         explicit GenericID( IDType _id ) : m_value( _id ) {}
         GenericID( const GenericID& _other ) : m_value( _other.m_value ) {}
         GenericID( const GenericID&& _other ) noexcept : m_value( _other.m_value ) {}
-        GenericID& operator = ( const GenericID& _other ) { m_value = _other.m_value; return *this; }
+        
+        GenericID&  operator =  ( const GenericID& _other ) { m_value = _other.m_value; return *this; }
+        bool        operator == ( const GenericID& _other ) const { return m_value == _other.m_value; }
+        bool        operator <  ( const GenericID& _other ) const { return m_value < _other.m_value; }
 
         IDType value() const { return m_value; }
         bool isValid() const { return InvalidValue != m_value; }
