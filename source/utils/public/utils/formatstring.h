f4342e807388fc7895d7669ff3316d760f9dde25
fpuma <fpuma88@gmail.com>
2021-03-19 18:39:52 +0100

    Changing file name

--- source/utils/public/utils/formatstring.h
+++ source/utils/public/utils/formatstring.h
@@ -5,6 +5,7 @@
 
 namespace puma
 {
+	///For now it will only print up to 256 characters
 	std::string formatString( const char* _format, ... )
 	{
 		va_list args;
@@ -12,6 +13,13 @@ namespace puma
 		char buffer[256];
 		vsnprintf( buffer, 256, _format, args );
 		va_end( args );
+		return std::string( buffer );
+	}
+
+	std::string formatString( const char* _format, const va_list& _args )
+	{
+		char buffer[256];
+		vsnprintf( buffer, 256, _format, _args );
 		return std::string( buffer );
 	}
 }
