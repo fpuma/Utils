00125f8bc58b2bb1af2310c1d43901d2fc9c1fe6
fpuma <fpuma88@gmail.com>
2021-10-11 17:50:35 +0200

    Added ContainedVector container. It is basically a custom container where we can dynamically add elements but once we try to add past the max count it asserts.

---
+++ source/utils/public/utils/containers/containedvector.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <array>
+#include <assert.h>
+#include <iterator>
+
+namespace puma
+{
+    template<class ElementType, std::size_t ContainerSize>
+    class ContainedVector
+    {
+    public:
+
+        using iterator = typename std::array<ElementType, ContainerSize>::iterator;
+        using const_iterator = typename std::array<ElementType, ContainerSize>::const_iterator;
+        using value_type = typename ElementType;
+
+        ElementType operator [] ( size_t _index ) const
+        {
+            assert( _index < size() );
+            return m_container[_index];
+        }
+
+        ElementType* data() { return m_container.begin(); }
+        const ElementType* data() const { return m_container.begin(); }
+
+        void push_back( const ElementType& _element )
+        {
+            assert( m_currentSize < ContainerSize ); //max size of container has been reached
+            m_container[m_currentSize] = _element;
+            ++m_currentSize;
+        }
+
+        void push_back( ElementType&& _element )
+        {
+            push_back( _element );
+        }
+
+        iterator erase(iterator _element) 
+        { 
+            assert( _element >= begin() ); // Iterator is outside of the container range
+            assert( _element < end() ); // Iterator is outside of the container range
+
+            size_t elementDistance = std::distance( m_container.begin(), _element );
+
+            if ( _element < (end() - 1) )
+            {
+                void* dst = (void*)(m_container.data() + elementDistance);
+                void* src = (void*)(m_container.data() + elementDistance + 1);
+                size_t copySize = std::distance( _element + 1, end() ) * sizeof(ElementType);
+                std::memcpy( dst, src, copySize );
+            }
+            --m_currentSize;
+
+            return m_container.begin() + elementDistance;
+        }
+
+        iterator begin() { return m_container.begin(); }
+        const_iterator begin() const { return m_container.begin(); }
+
+        iterator end() { return m_container.begin() + m_currentSize; }
+        const_iterator end() const { return m_container.begin() + m_currentSize; }
+
+        size_t size() const { return m_currentSize; }
+        
+        void clear() { m_currentSize = 0; }
+
+    private:
+
+        std::array<ElementType, ContainerSize> m_container;
+        size_t m_currentSize = 0;
+
+    };
+}
---
+++ source/utilstests/tests/containers/containedvectortest.cpp
@@ -0,0 +1,36 @@
+#include "containedvectortest.h"
+
+using namespace puma;
+
+TEST( Containers, ContainedVector )
+{
+    ContainedVector<int, 5> container;
+
+    EXPECT_EQ( container.size(), 0 );
+
+    container.push_back( 0 );
+    container.push_back( 1 );
+    container.push_back( 2 );
+
+    EXPECT_EQ( container.size(), 3 );
+
+    container.push_back( 3 );
+    container.push_back( 4 );
+
+    EXPECT_EQ( container.size(), 5 );
+
+    EXPECT_DEATH( container.push_back( 0 ), "m_currentSize < ContainerSize" );
+
+    container.erase( container.begin() + 2 );
+
+    EXPECT_EQ( container.size(), 4 );
+
+    EXPECT_EQ( container[0], 0 );
+    EXPECT_EQ( container[1], 1 );
+    EXPECT_EQ( container[2], 3 );
+    EXPECT_EQ( container[3], 4 );
+
+    container.clear();
+
+    EXPECT_EQ( container.size(), 0 );
+}
