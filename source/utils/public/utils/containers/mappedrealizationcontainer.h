c835a7294ddf7e95a33573bce06998ff245754c7
fpuma <fpuma88@gmail.com>
2023-03-10 00:38:42 +0100

    Allowing access to elements through type indexes on mapped realization container

--- source/utils/public/utils/containers/mappedrealizationcontainer.h
+++ source/utils/public/utils/containers/mappedrealizationcontainer.h
@@ -39,113 +39,12 @@ namespace puma
             return *this;
         }
 
-        template<class T>
-        std::shared_ptr<T> add( Key _key )
-        {
-            static_assert(std::is_base_of<BaseClass, T>::value);
-
-            auto itElement = m_elements.find( _key );
-            
-            if ( m_elements.end() == itElement )
-            {
-                auto emplaceResult = m_elements.emplace( _key, std::move( m_containerRegistryTemplate.cloneRegistriesOnly() ) );
-                assert( emplaceResult.second );
-                if (!emplaceResult.second) return nullptr;
-
-                itElement = emplaceResult.first;
-            }
-
-            auto result = itElement->second.add<T>();
-
-            onAdded( _key, result, std::type_index( typeid(T) ) );
-            return result;
-        }
-
-        template<class T>
-        T* get( Key _key )
-        {
-            auto itElement = m_elements.find( _key );
-            assert( itElement != m_elements.end() ); //There are no elements for _key
-            if (itElement == m_elements.end()) return nullptr;
-
-            return itElement->second.getSafely<T>().get();
-        }
-
-        template<class T>
-        const T* get( Key _key) const
-        {
-            auto itElement = m_elements.find( _key );
-            assert( itElement != m_elements.end() ); //There are no elements for _key
-            if (itElement == m_elements.end()) return nullptr;
-
-            return itElement->second.getSafely<T>().get();
-        }
-
-        template<class T>
-        std::shared_ptr<T> getSafely( Key _key )
-        {
-            auto itElement = m_elements.find( _key );
-            assert( itElement != m_elements.end() ); //There are no elements for _key
-            if (itElement == m_elements.end()) return nullptr;
-
-            return itElement->second.getSafely<T>();
-        }
-
-        template<class T>
-        std::shared_ptr<const T> getSafely( Key _key ) const
-        {
-            auto itElement = m_elements.find( _key );
-            assert( itElement != m_elements.end() ); //There are no elements for _key
-            if (itElement == m_elements.end()) return nullptr;
-
-            return itElement->second.getSafely<T>();
-        }
-
-        template<class T>
-        void remove( Key _key )
-        {
-            static_assert(std::is_base_of<BaseClass, T>::value);
-
-            auto itElement = m_elements.find( _key );
-            assert( itElement != m_elements.end() ); //There are no elements for _key
-            
-            if (itElement != m_elements.end())
-            {
-                onRemoved( _key, itElement->second.getSafely<T>(), std::type_index(typeid(T)));
-                itElement->second.remove<T>();
-                if (itElement->second.size() == 0)
-                {
-                    m_elements.erase( itElement );
-                }
-            }
-        }
-
-        bool contains( Key _key ) const
-        {
-            return m_elements.contains( _key );
-        }
-
-        template<class T>
-        bool contains( Key _key ) const
-        {
-            bool result = false;
-
-            auto itElement = m_elements.find( _key );
-
-            if ( m_elements.end() != itElement )
-            {
-                result = itElement->second.contains<T>();
-            }
-
-            return result;
-        }
-
         template<class InterfaceClass, class RealizedClass>
         void registerInterface()
         {
             m_containerRegistryTemplate.registerInterface<InterfaceClass, RealizedClass>();
 
-            for ( auto itElement = m_elements.begin(); itElement != m_elements.end(); ++itElement )
+            for (auto itElement = m_elements.begin(); itElement != m_elements.end(); ++itElement)
             {
                 itElement->second.registerInterface<InterfaceClass, RealizedClass>();
             }
@@ -156,7 +55,7 @@ namespace puma
         {
             m_containerRegistryTemplate.registerClass<RealizedClass>();
 
-            for ( auto itElement = m_elements.begin(); itElement != m_elements.end(); ++itElement )
+            for (auto itElement = m_elements.begin(); itElement != m_elements.end(); ++itElement)
             {
                 itElement->second.registerClass<RealizedClass>();
             }
@@ -166,6 +65,145 @@ namespace puma
         bool isRegistered() const
         {
             return m_containerRegistryTemplate.isRegistered<ClassToCheck>();
+        }
+
+        template<class T>
+        std::shared_ptr<T> add( Key _key )
+        {
+            static_assert(std::is_base_of<BaseClass, T>::value);
+            return static_pointer_cast<T>(add( _key, std::type_index( typeid(T) ) ));
+        }
+
+        template<class T>
+        void remove( Key _key )
+        {
+            static_assert(std::is_base_of<BaseClass, T>::value);
+            remove( _key, std::type_index( typeid(T) ) );
+        }
+
+        template<class T>
+        T* get( Key _key )
+        {
+            static_assert(std::is_base_of<BaseClass, T>::value);
+            return static_cast<T*>(get( _key, std::type_index( typeid(T) ) ));
+        }
+
+        template<class T>
+        const T* get( Key _key ) const
+        {
+            static_assert(std::is_base_of<BaseClass, T>::value);
+            return static_cast<const T*>(get( _key, std::type_index( typeid(T) ) ));
+        }
+
+        template<class T>
+        std::shared_ptr<T> getSafely( Key _key )
+        {
+            static_assert(std::is_base_of<BaseClass, T>::value);
+            return static_pointer_cast<T>(getSafely( _key, std::type_index( typeid(T) ) ));
+        }
+
+        template<class T>
+        std::shared_ptr<const T> getSafely( Key _key ) const
+        {
+            static_assert(std::is_base_of<BaseClass, T>::value);
+            return static_pointer_cast<const T>(getSafely( _key, std::type_index( typeid(T) ) ));
+        }
+
+        bool contains( Key _key ) const
+        {
+            return m_elements.contains( _key );
+        }
+
+        template<class T>
+        bool contains( Key _key ) const
+        {
+            static_assert(std::is_base_of<BaseClass, T>::value);
+            return contains( _key, std::type_index( typeid(T) ) );
+        }
+
+        std::shared_ptr<BaseClass> add( Key _key, std::type_index _typeIndex )
+        {
+            auto itElement = m_elements.find( _key );
+
+            if (m_elements.end() == itElement)
+            {
+                auto emplaceResult = m_elements.emplace( _key, std::move( m_containerRegistryTemplate.cloneRegistriesOnly() ) );
+                assert( emplaceResult.second );
+                if (!emplaceResult.second) return nullptr;
+
+                itElement = emplaceResult.first;
+            }
+
+            auto result = itElement->second.add( _typeIndex );
+
+            onAdded( _key, result, _typeIndex );
+            return result;
+        }
+
+        void remove( Key _key, std::type_index _typeIndex )
+        {
+            auto itElement = m_elements.find( _key );
+            assert( itElement != m_elements.end() ); //There are no elements for _key
+
+            if (itElement != m_elements.end())
+            {
+                onRemoved( _key, itElement->second.getSafely( _typeIndex ), _typeIndex );
+                itElement->second.remove( _typeIndex );
+                if (itElement->second.size() == 0)
+                {
+                    m_elements.erase( itElement );
+                }
+            }
+        }
+
+        BaseClass* get( Key _key, std::type_index _typeIndex )
+        {
+            auto itElement = m_elements.find( _key );
+            assert( itElement != m_elements.end() ); //There are no elements for _key
+            if (itElement == m_elements.end()) return nullptr;
+
+            return itElement->second.getSafely( _typeIndex ).get();
+        }
+
+        const BaseClass* get( Key _key, std::type_index _typeIndex ) const
+        {
+            auto itElement = m_elements.find( _key );
+            assert( itElement != m_elements.end() ); //There are no elements for _key
+            if (itElement == m_elements.end()) return nullptr;
+
+            return itElement->second.getSafely( _typeIndex ).get();
+        }
+
+        std::shared_ptr<BaseClass> getSafely( Key _key, std::type_index _typeIndex )
+        {
+            auto itElement = m_elements.find( _key );
+            assert( itElement != m_elements.end() ); //There are no elements for _key
+            if (itElement == m_elements.end()) return nullptr;
+
+            return itElement->second.getSafely( _typeIndex );
+        }
+
+        std::shared_ptr<const BaseClass> getSafely( Key _key, std::type_index _typeIndex) const
+        {
+            auto itElement = m_elements.find( _key );
+            assert( itElement != m_elements.end() ); //There are no elements for _key
+            if (itElement == m_elements.end()) return nullptr;
+
+            return itElement->second.getSafely( _typeIndex );
+        }
+
+        bool contains( Key _key, std::type_index _typeIndex ) const
+        {
+            bool result = false;
+
+            auto itElement = m_elements.find( _key );
+
+            if (m_elements.end() != itElement)
+            {
+                result = itElement->second.contains( _typeIndex );
+            }
+
+            return result;
         }
 
         void visit( Key _key, std::function<void( std::shared_ptr<BaseClass> )> _function )
